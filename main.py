e9d6fc03c972b4ed496bc69a610afdbd5ccfe1cd
vlad4 <vlad4.efremova@yandex.ru>
2024-11-27 00:20:37 +0300

    Сделала второе окно

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tkinter
+import random
 
 class Interface:
     def __init__(self):
         self._main_window = tkinter.Tk()
         self._main_window.title('Угадай число')
         self._main_window.geometry("400x400")
-        self._label = tkinter.Label(self._main_window,text = 'Это игра "Угадай число"!  Выбери,кто будет загадывать:',
+        self._label = tkinter.Label(self._main_window,text = 'Это игра "Угадай число"!\nВыбери,кто будет загадывать:',
                                     font=("Arial", 8, "bold"))
         self._label.pack()
         self._top_fame = tkinter.Frame()
@@ -13,11 +14,28 @@ class Interface:
         self._first_button = tkinter.Button(self._top_fame, text='Число загадывает компьютер',
                                             command=self.game_start)
         self._first_button.pack()
-        self._second_button = tkinter.Button(self._top_fame, text='Число загадывает компьютер',
+        self._second_button = tkinter.Button(self._top_fame, text='Число загадывает пользователь',
                                              command=self.game_start)
         self._second_button.pack()
 
-        tkinter.mainloop()
+        self._main_window.mainloop()
+
+    def game_start(self):
+        self._main_window.destroy()
+        secret_number = random.randint(1, 100)
+        attempts = 0
+        said_window = tkinter.Tk()
+        said_window.title('Угадай число')
+        said_window.geometry("400x400")
+        label1 = tkinter.Label(said_window, text='Игра началась!\nКомпьютер загадал число от 1 до 100',
+                              font=("Arial", 10, "bold"))
+        label1.pack()
+        enter = tkinter.Entry(said_window, width=30)
+        enter.pack()
+        third_button = tkinter.Button(said_window, text='Проверить')
+        third_button.pack()
+
+
 
 if __name__ == '__main__':
     game = Interface()
